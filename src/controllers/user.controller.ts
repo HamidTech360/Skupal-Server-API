e1f397fa408644ad07cf19549a39c62ba9dfc68a
HamidTech360 <hammedowolabi2001@gmail.com>
2022-03-29 03:52:39 -1100

    adding auth and get User endpoint

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import joi from 'joi-browser'
 import {CONFIG} from '../config'
 const config = CONFIG()
 import { ValidateUser,  UserModel } from "../models/user.model"
@@ -38,7 +39,7 @@ export const createUser = async (req:any, res:any, next:any)=>{
         const email_body= `
         <div>
             <div>Welcome to SKUPAL. We hope to serve you with the best freelancing experience</div>
-            <div> click <a href="https://easytopup.netlify.app/verify_account/${newUser.confirmationCode}">HERE</a> to verify your account</div>
+            <div> click <a href="https://skupal.netlify.app/verify_account/${newUser.confirmationCode}">HERE</a> to verify your account</div>
         </div>
         `
         sendMail(req.body.email, 'Skupal Team', email_body)
@@ -47,4 +48,72 @@ export const createUser = async (req:any, res:any, next:any)=>{
     }
 
     
+}
+
+export const AuthUser = async (req:any, res:any, next:any)=>{
+    const {error} = ValidateAuth(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+
+    try{
+        const checkUser = await UserModel.findOne({email:req.body.email})
+        if(!checkUser) return res.status(401).send('User not found')
+
+        const checkPwd = await bcrypt.compare(req.body.password, checkUser.password)
+        if(!checkPwd) return res.status(401).send('Invalid password')
+        
+        
+        
+        const token= jwt.sign({...checkUser}, `${config.JWT_SECRET}`)
+     
+        
+        res.json({
+            status:'success',
+            message:'Login successful',
+            Auth_token:token,
+           
+        })
+      
+    }catch(ex){
+        res.status(500).send('Server Error')
+    }
+}
+
+export const verifyAccount = async (req:any, res:any, next:any)=>{
+    const {ref} = req.body
+    try{
+        const checkCode = await UserModel.findOne({confirmationCode:ref})
+        if(!checkCode) return res.status(400).send('Failed to verify account')
+
+        checkCode.status = "verified"
+        checkCode.save()
+        res.json({
+            status:'success',
+            message:'Account successfully verified'
+        })
+    }catch(ex){
+        res.status(500).send('Server Error. Cannot verify account')
+    }
+}
+
+export const getUser = async (req:any, res:any, next:any)=>{
+    try{      
+        const user = await UserModel.findById(req.user._doc._id)
+        user.password=""
+        user.confirmationCode=""
+        res.json({
+            status:'success',
+            data:user
+        })
+    }catch(error){
+        res.status(403).send(error)
+    }
+}
+
+function ValidateAuth (user:any){
+    const schema = {
+        email:joi.string().required(),
+        password:joi.string().required()
+    }
+    return joi.validate(user, schema)
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,11 @@
 import express from "express";
 const router = express.Router()
-import {createUser} from '../controllers/user.controller'
+import Auth from '../middlewares/auth'
+import {createUser, AuthUser, getUser, verifyAccount} from '../controllers/user.controller'
 
 router.post('/', createUser)
+router.get('/', Auth, getUser)
+router.post('/auth', AuthUser)
+router.post('/verify', verifyAccount)
 
 export default router;
